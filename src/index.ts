560805f839729a44f0b7e4bfc495631850b7e24c
“Mohsin <mohsinsha@gmail.com>
2026-05-25 12:48:56 +0530

    changes

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ app.listen(4040, () => {
 
 async function sendTestMessage(): Promise<void> {
   const htmlMessage = `
-  <b>Hello <i>Rajesh</i>  👋</b>
+  <b>Hello <i>Mohsin</i>  👋</b>
 You are successfully onboarded 👍
 
 Your order has been <u>successfully placed</u>.
@@ -26,8 +26,8 @@ Your order has been <u>successfully placed</u>.
 
 <a href="https://example.com">View Order</a>
 `;
-  // await sendTelegramMessage("7443019493", htmlMessage); //7443019493 = mohsin
+  await sendTelegramMessage("7443019493", htmlMessage); //7443019493 = mohsin
   // await sendTelegramMessage("1392132707", htmlMessage); //1392132707 = rashee
   // await sendTelegramMessage("1057925473", htmlMessage); //1057925473 = vaibhav
-  await sendTelegramMessage("8730608762", htmlMessage); //8730608762 = Rajesh
+  // await sendTelegramMessage("8730608762", htmlMessage); //8730608762 = Rajesh
 }
